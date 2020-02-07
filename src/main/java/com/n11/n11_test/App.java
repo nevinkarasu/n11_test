681c97ebaf1eff30904d1c244e2d3e5d6428b5e4
nevinkarasu <nevinkarasu@users.noreply.github.com>
2020-02-07 13:35:04 +0300

    project pertama

---
+++ src/main/java/com/n11/n11_test/App.java
@@ -0,0 +1,25 @@
+package com.n11.n11_test;
+
+
+/*import java.util.Iterator;
+import java.util.logging.Level;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.logging.LogEntries;
+import org.openqa.selenium.logging.LogEntry;
+import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;*/
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+    }
+
+}
